419068b020ce55002be70e7f0b4dc2f781d96cc5
Divya Gupta <dg379804@gmail.com>
2024-01-09 20:19:33 +0530

    c programming

---
+++ scope.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int n=100;
+int main()
+{
+    int n=1;
+    {
+        int k=23;
+        int n=5;
+    cout<<"we're in inner block";
+    }
+}
---
+++ variable.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a; // variable declaration
+a=12 ;//variable initialization
+
+cout<<"size of int"<<sizeof(a)<<endl;
+float b;
+cout<<"size of float"<<sizeof(b)<<endl;
+char c;
+cout<<"size of character"<<sizeof(c)<<endl;
+}
