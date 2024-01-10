52a87f1d1ca3acb23af9fa475aa6ba8134e37539
Divya Gupta <dg379804@gmail.com>
2024-01-10 19:19:40 +0530

    c++

---
+++ while_loop.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"Enter number for list";
+    cin>>n;
+    while (i<=n)
+    {
+       
+        i=i+1;
+        int sum=0;
+        sum=sum+i;
+        cout<<i<<endl;
+    }
+    
+}
---
+++ while_loop_.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"Enter number "<<endl;
+    cin>>n;
+    while (i%2==0)
+    {
+        cout<<i;
+        i=i+1;
+    }
+    
+}
